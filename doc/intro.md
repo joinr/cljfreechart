a4e790598baf1e96b6ad0185a2f1554c8337cb7a
tom <tom@that-place>
2018-05-19 04:27:45 -0500

    Initial project build

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljfreechart
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cljfreechart/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljfreechart.core-test
+  (:require [clojure.test :refer :all]
+            [cljfreechart.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
