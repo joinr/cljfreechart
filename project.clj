3dc113357431204ace1570d4483d3f732412f414
tom <tom@the-cloud>
2018-10-22 19:44:50 +0000

    Added legacy charts.clj to reflect jfree1.5 deltas

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  [org.clojure/test.check "0.9.0"]
                  [org.jfree/jfreechart "1.5.0"]
                  [metosin/spec-tools "0.7.0"]
+                 [joinr/incanter "1.9.3-SNAPSHOT" :exclude [org.jfree/jfreechart]]
                  ])
